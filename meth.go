bdafc0b3f594967ff2b1a47992c14bae4ce49e2a
Hiram J. Pérez <worg@linuxmail.org>
2014-12-07 21:38:06 -0600

    implement methods to work with result sets. Closes #2

--- meth.go
+++ meth.go
@@ -34,9 +34,71 @@ var (
 	ErrNoSlice = errors.New(`slice expected`)
 )
 
-// Persistent is the interface that allows us to communicate with upper.io/db collection
-type Persistent interface {
-	Collection() db.Collection
+type (
+	// Persistent is the interface that allows us to communicate with upper.io/db collection
+	Persistent interface {
+		Collection() db.Collection
+	}
+	// resFunc is a func type to operate over results
+	resFunc func(db.Result)
+)
+
+// The next functions are basically alias for their upper counterparts
+// the documentation is mostly stolen from upper
+
+// Limit defines the maximum number of results, returns a resFunc usable on AllOp
+func Limit(u uint) resFunc {
+	return func(r db.Result) {
+		r.Limit(u)
+	}
+}
+
+// Skip ignores the first *n* results, returns a resFunc usable on AllOp
+func Skip(n uint) resFunc {
+	return func(r db.Result) {
+		r.Skip(n)
+	}
+}
+
+// Sort receives field names that define the order in which elements will
+// be returned in a query, field names may be prefixed with a minus sign (-)
+// indicating descending order; ascending order would be used by default.
+// also returns a resFunc
+func Sort(i ...interface{}) resFunc {
+	return func(r db.Result) {
+		r.Sort(i...)
+	}
+}
+
+// Select defines specific fields to be fulfilled on results in this result
+// set.
+func Select(i ...interface{}) resFunc {
+	return func(r db.Result) {
+		r.Select(i...)
+	}
+}
+
+// Where discards the initial filtering conditions and sets new ones.
+func Where(i ...interface{}) resFunc {
+	return func(r db.Result) {
+		r.Where(i...)
+	}
+}
+
+// Group is used to group results that have the same value in the same
+// column or columns.
+func Group(i ...interface{}) resFunc {
+	return func(r db.Result) {
+		r.Group(i...)
+	}
+}
+
+// Paginate applies a limit and skip to a result set
+func Paginate(limit, skip uint) resFunc {
+	return func(r db.Result) {
+		r.Limit(limit)
+		r.Skip(skip)
+	}
 }
 
 // One fills p with one element based on the id field
@@ -90,9 +152,8 @@ func All(p Persistent, rows interface{}, cond ...interface{}) error {
 func Exists(p Persistent, cond ...interface{}) bool {
 	col := p.Collection()
 
-	id, _ := getID(p)
-
 	if len(cond) < 1 {
+		id, _ := getID(p)
 		cond = []interface{}{db.Cond{`id`: id}}
 	}
 
@@ -103,11 +164,38 @@ func Exists(p Persistent, cond ...interface{}) bool {
 	return count > 0
 }
 
+// AllOp works like All but applies an operation in form of resFunc [func(*db.Result)] to work
+// with the result set [for things like select, group, limit]
+func AllOp(p Persistent, operation resFunc, rows interface{}, cond ...interface{}) error {
+	col := p.Collection()
+
+	res := col.Find(cond...)
+
+	operation(res)
+
+	err := res.All(rows)
+
+	return err
+}
+
+// OneOp works like AllOp but returns only one row
+func OneOp(p Persistent, operation resFunc, row interface{}, cond ...interface{}) error {
+	col := p.Collection()
+
+	res := col.Find(cond...)
+
+	operation(res)
+
+	err := res.One(row)
+
+	return err
+}
+
 func getID(p interface{}) (int64, error) {
 	valP := getValue(p)
 	i := util.GetStructFieldIndex(valP.Type(), `id`)
 	if len(i) != 1 {
-		return 0, ErrNoId
+		return 0, ErrNoID
 	}
 
 	id := valP.FieldByIndex(i).Int()
